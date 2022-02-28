422cc8bad96eb8ae1a92915f93121340e19d3d25
Codexive_zech <zechariahhounwanou@gmail.com>
2022-02-28 21:32:18 +0100

    Added MySQL and inserted seeding data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into country value(1, 'Washington DC', 'USA', 'North America', 'United State', 'American');
+insert into country value(2, 'Mumbai', 'IN', 'Asia', 'India', 'Indian');
+insert into country value(3, 'Abuja', 'NGN', 'Africa', 'Nigeria', 'Nigerian');
